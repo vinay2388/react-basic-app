21dd824bb84ca5bb4eb49074ce2c7d8169f720b2
vinay.reddy <vinay.reddy@stackaero.com>
2020-09-28 17:20:29 +0530

    updated

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [  -z "${username}" ]; then
-  gitUrl=https://github.com/vinay2388/react-basic-app.git
-fi
-git clone $gitUrl /opt/apps/
+# if [  -z "${username}" ]; then
+  # gitUrl=https://github.com/vinay2388/react-basic-app.git
+# fi
+git clone https://github.com/vinay2388/react-basic-app.git /opt/apps/
 cd /opt/apps/
 npm install
