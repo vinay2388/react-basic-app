4fbaf4eadc8f11d2a77f23e9a577348c82366743
vinay.reddy <vinay.reddy@stackaero.com>
2020-09-30 16:42:36 +0530

    script updated

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ -z "${username}" ] && [ -z "${password}"] then
   gitUrl=https://github.com/vinay2388/react-basic-app.git
 else
-  echo 'user not valid'
+  echo "user not valid"
 fi
 echo $gitUrl
 git clone ${gitUrl} /opt/apps/react
