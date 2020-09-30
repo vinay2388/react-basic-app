80457054de0c267c906b8f0b152421eb8c89d0f4
vinay.reddy <vinay.reddy@stackaero.com>
2020-09-30 16:20:39 +0530

    script updated

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 if [[ -f "${username}" && -f "${password}" ]]; then
   gitUrl=https://github.com/vinay2388/react-basic-app.git
 fi
+echo $gitUrl
 git clone ${gitUrl} /opt/apps/react
 cp /opt/apps/react/script.sh /opt/apps
 cd /opt/apps/react
