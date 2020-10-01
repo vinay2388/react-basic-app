4baf104b49b73c711d7c6a61cc626767a3e4d8cd
vinay.reddy <vinay.reddy@stackaero.com>
2020-10-01 11:28:50 +0530

    build.sh file added

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ -z "${username}" && -z "${password}" ]]; then
+  gitUrl=$giturl
+fi
+echo $gitUrl
+git clone ${gitUrl} /opt/apps/react
+# cp /opt/apps/react/script.sh /opt/apps
+cd /opt/apps/react
+npm install
+npm run build
+mv build /opt/apps
+mv package.json /opt/apps
+cd /opt/apps/
+rm -rf react
